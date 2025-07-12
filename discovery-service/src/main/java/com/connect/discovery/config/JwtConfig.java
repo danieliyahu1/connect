89f7d30f66cb297e9d49a4ae1c081143a07de6c8
danieliyahu1 <danieliyahu1@gmail.com>
2025-07-12 17:55:29 +0300

    adding auth common & dockerfile

---
+++ discovery-service/src/main/java/com/connect/discovery/config/JwtConfig.java
@@ -0,0 +1,24 @@
+package com.connect.discovery.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+@Configuration
+public class JwtConfig {
+
+        @Bean
+        public PublicKey publicKey(@Value("${jwt.public.key}") String publicKeyBase64) throws NoSuchAlgorithmException, InvalidKeySpecException {
+            byte[] keyBytes = Base64.getDecoder().decode(publicKeyBase64);
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            return factory.generatePublic(spec);
+        }
+}
---
+++ discovery-service/src/main/java/com/connect/discovery/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.connect.discovery.config;
+
+import com.connect.auth.common.config.CommonSecurityConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CommonSecurityConfig commonSecurityConfig;
+
+    @Bean
+    public SecurityFilterChain connectorSecurityFilterChain(HttpSecurity http) throws Exception {
+        commonSecurityConfig.commonSecurityFilterChain(http);
+        return http.build();
+    }
+}
